6390055b324ffed1705a7b7406bbc6827603f83f
Capstone11111 <102351345+Capstone11111@users.noreply.github.com>
2022-04-02 09:24:41 +0800

    Update login.php

--- login/connect/login.php
+++ login/connect/login.php
@@ -1,5 +1,5 @@
 <?php
-     $link = mysqli_connect("localhost","BluJKp71nn","l8RqOwTB9g","BluJKp71nn");// server, user, password, database
+     $link = mysqli_connect("localhost","BluJKp71nn","8SM1oD3Vsv","BluJKp71nn");// server, user, password, database
 
 
      if (mysqli_connect_error()){
