1a52294a0931a6c9a3f3254c81ac4f6c9086862b
Capstone11111 <102351345+Capstone11111@users.noreply.github.com>
2022-04-04 01:24:51 +0800

    Update dashboard.php

--- login/dashboard.php
+++ login/dashboard.php
@@ -1,5 +1,5 @@
 <?php
-     $link = mysqli_connect("localhost","X6awpxRRoa","jLhKLl0WRL","X6awpxRRoa");// server, user, password, database
+    $link = mysqli_connect("remotemysql.com","X6awpxRRoa","jLhKLl0WRL","X6awpxRRoa");// server, user, password, database
      ?>
      
        <?php
