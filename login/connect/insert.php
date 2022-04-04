a6562685844f87f5c8a7492266c182a40219e911
Capstone11111 <102351345+Capstone11111@users.noreply.github.com>
2022-04-04 19:43:00 +0800

    Add files via upload

---
+++ login/connect/insert.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html >
+  <body>
+    <form class="box" method="post" action="formaction.php" autocomplete="off">
+    
+      <input type="number" placeholder="Enter value" name="fire" required>
+        <input type="number"  placeholder="Enter value" name="gsm" id="myInput"required>
+          <input type="submit" value="insert" >
+    </form>
+  </body>
+</html>
