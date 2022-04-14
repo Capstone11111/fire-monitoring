0832bbc8823d53664a5741ade8f55524c80423a5
Capstone11111 <102351345+Capstone11111@users.noreply.github.com>
2022-04-14 08:49:14 +0800

    Update login.php

--- login/connect/login.php
+++ login/connect/login.php
@@ -1,25 +1,23 @@
 <?php
-     $link = mysqli_connect("remotemysql.com","X6awpxRRoa","jLhKLl0WRL","X6awpxRRoa");// server, user, password, database
-
+    $link = mysqli_connect("remotemysql.com","X6awpxRRoa","jLhKLl0WRL","X6awpxRRoa");// server, user, password, database
 
      if (mysqli_connect_error()){
        die("there was an error connecting");
      }
-if(isset($_POST["btn"])){
-  $err=" Incorrect Username/ password";
-$Username = $_POST['username'];
-$Password = $_POST['password'];
-$_SESSION["status"]=false;
+session_start();
+$username1 = $_POST['username'];
+$password1 = $_POST['password'];
 
 $query = "SELECT `username`, `password` FROM `users` WHERE BINARY `username`='$Username' AND BINARY `password` ='$Password'";
 $result = mysqli_query($link,$query);
-$_SESSION["status"]=true;
-
 if(mysqli_num_rows($result)== 1){
+
+$_SESSION['username'] = $username1;
 header('location:../dashboard.php');
+
 } else{
-   // echo $err;
- header('location:../loginpage.php');
-}
+header('location:../loginpage.php');
+
+
 }
 ?>
