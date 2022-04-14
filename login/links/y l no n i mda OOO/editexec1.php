552ecf211f3f04badd26d3ae6b5b1f42bc268e8e
Capstone11111 <102351345+Capstone11111@users.noreply.github.com>
2022-04-14 09:32:58 +0800

    Update editexec1.php

--- login/links/y l no n i mda OOO/editexec1.php
+++ login/links/y l no n i mda OOO/editexec1.php
@@ -4,16 +4,15 @@
      if (mysqli_connect_error()){
        die("there was an error connecting");
      }
-session_start();
 $id=$_POST['id'];
 $username = $_POST['username'];
 $password = $_POST['password'];
 // $code = $_POST['code'];
 
-$query ="UPDATE `admin` SET `A_User`='$username',`A_Pass`='$password' WHERE `id`= '$id'";
+$query ="UPDATE `admin` SET `A_User`='$username',`A_Pass`='$password' WHERE `id`= 1";
 $result = mysqli_query($link,$query);
 if ($result) {
-header('location:admindashboardmod.');
+header('location:admindashboardmod.php');
 } else {
 echo "fail";
 }
